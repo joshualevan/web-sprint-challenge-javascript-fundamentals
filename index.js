5ccd900f13cee27252bb0f64a19cfa379ed45911
Josh Levan <joshlevan@Joshs-iMac-2.local>
2021-03-26 13:52:47 -0400

    MVP now complete- broke code when uncommented lines 213 and 214 per instructions

--- index.js
+++ index.js
@@ -210,8 +210,8 @@ class CuboidMakerTwo {
 
 
 //🦄🦄🦄 Test your volume and surfaceArea methods by uncommenting the logs below: 🦄🦄🦄
-console.log(cuboidTwo.volume()); // 100
-console.log(cuboidTwo.surfaceArea()); // 130
+// console.log(cuboidTwo.volume()); // 100
+// console.log(cuboidTwo.surfaceArea()); // 130
 
 
 
